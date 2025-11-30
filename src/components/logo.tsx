2451ad089f4bd6ad2f73f145039843865dd549f9
Joe Davies <jwd101@gmail.com>
2025-11-30 13:37:48 +0100

    christmas theme

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,13 +2,17 @@ import { Flex, Title, Image } from '@mantine/core';
 import { IconChartScatter } from '@tabler/icons-react';
 
 export function Logo() {
+  const christmas =
+    (new Date().getMonth() === 11 && new Date().getDate() >= 1) || // December (month 11)
+    (new Date().getMonth() === 0 && new Date().getDate() <= 7);   // January (month 0)
+
   return (
     <Flex align={'center'} gap={4} justify={'center'}>
       <IconChartScatter />
       <Title fw={700} order={2} c="var(--mantine-white)">
         STATLE
       </Title>
-      <Image
+      {christmas && <Image
         src="./img/santa-hat.png"
         alt="Santa hat"
         width={25}
@@ -17,7 +21,7 @@ export function Logo() {
           transform: 'translate(-20px, -5px)',
           pointerEvents: 'none',
         }}
-      />
+      />}
     </Flex>
   );
 }
